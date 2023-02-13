9f9c858bd577fc651c08f93604206ce77f583fdf
Natthaphon <n.foithong1983@gmail.com>
2023-02-13 16:28:08 -0500

    find a single subject by id

--- Sites/globe_bank/private/query_functions.php
+++ Sites/globe_bank/private/query_functions.php
@@ -10,6 +10,19 @@
 		return $result;
 	}
 	
+	function find_subject_by_id($id) {
+		global $db;
+		
+		$sql = "SELECT * FROM subjects ";
+		$sql .= "WHERE id='" . $id . "'";
+		$result = mysqli_query($db, $sql);
+		confirm_result_set($result);
+		
+		$subject = mysqli_fetch_assoc($result);
+		mysqli_free_result($result);
+		return $subject; // return an assoc. array
+	}
+	
 	function find_all_pages() {
 		global $db;
 		
